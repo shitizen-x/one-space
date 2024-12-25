91658ef423c0b569d78df3348f5ae6e1a140c49f
shitizen-x <wdsmzx@gmail.com>
2024-12-25 15:52:12 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "One Space",
-  description: "One Space",
+  title: "XHH Space",
+  description: "XHH Space",
   icons: {
     icon: "/favicon.svg"
   }
